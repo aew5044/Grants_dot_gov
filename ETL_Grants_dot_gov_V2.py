f1eb9044e28327d5062752a09962fdda0bbe88bf
Walker Texas <55103904+aew5044@users.noreply.github.com>
2023-11-12 07:30:27 -0500

    Create PDF
    Started the reportlab process to create the PDF for the newsletter.

--- ETL_Grants_dot_gov_V2.py
+++ ETL_Grants_dot_gov_V2.py
@@ -39,16 +39,9 @@ Aew5044@gmail.com
 
 download_space = r"C:\Users\aew50\Downloads"
 py_space = r"C:\Python\Grants_dot_gov"
-import pandas as pd
-import plotly.express as px
-# import requests
-# import xml.etree.ElementTree as ET
-from datetime import datetime
-# import zipfile
-# import os
 
 #User defined functions
-from download_and_clean_raw_data import global_variables, download, clean_df
+from download_and_clean_raw_data import global_variables, download, clean_df, create_line_chart, create_pdf
 
 df_clean = None
 #%%
@@ -63,84 +56,12 @@ def main():
     else:
         print('clean_df already exists')
 
+    create_line_chart(df_clean, today_date)
+
+    create_pdf(df_clean, today_date)
+
 if __name__ == "__main__":
     main()
 
-# %%
-#this is the method to query the dataframe based on the column name and value
-today = datetime.today()
-
-DE = df_clean.query('AgencyName == "Department of Education" \
-              and CloseDate >= @today')
-
-#This is the method to format and sum the EstimatedTotalProgramFunding column
-#and print the results to the console for easy sharing.
-formatted_funding = "${:,.0f}".format(DE["EstimatedTotalProgramFunding"].sum())
-print(f'There is a possible Estimated Total ProgramFunding of {formatted_funding} for the Department of Education available as of {today.strftime("%m/%d/%Y")}')
 
 # %%
-#Filter out all rows where the CloseDate is greater then today's date
-#Group by AgencyName and sum the EstimatedTotalProgramFunding
-#Sort the values in descending order
-
-today = datetime.today()
-agencies = df_clean \
-            .query('CloseDate >= @today') \
-            .groupby('AgencyName') \
-            .agg({'EstimatedTotalProgramFunding': 'sum'}) \
-            .sort_values(by='EstimatedTotalProgramFunding', ascending=False) \
-            .reset_index() \
-            .assign(in_MM = lambda x: x.EstimatedTotalProgramFunding/1000000) 
-            
-agencies = agencies[agencies['EstimatedTotalProgramFunding'] > 0]
-
-agencies
-
-# %%
-
-# Load the dataset
-file_path = r"C:\Python\Grants_dot_gov\GrantsDBExtract20211006v2.csv"  # Update with the path to your data file
-grants_data = pd.read_csv(file_path)
-
-# Convert CloseDate to datetime and calculate the number of days from today
-grants_data['CloseDate'] = pd.to_datetime(grants_data['CloseDate'], errors='coerce')
-today = datetime.now()
-grants_data['DaysUntilClose'] = (grants_data['CloseDate'] - today).dt.days
-
-# # Handle NaN values in 'EstimatedTotalProgramFunding'
-filtered_data = grants_data.dropna(subset=['EstimatedTotalProgramFunding'])
-
-# Group by AgencyName and sum the EstimatedTotalProgramFunding, then take the top 20
-top_agencies = filtered_data.groupby('AgencyName')['EstimatedTotalProgramFunding'].sum().nlargest(20).index
-
-# Filter the dataset for only these top agencies
-filtered_data = filtered_data[filtered_data['AgencyName'].isin(top_agencies)].query('CloseDate >= @today')    
-filtered_data2 = filtered_data[filtered_data['DaysUntilClose'] > 0]
-
-# Create the bubble plot with the adjusted data
-fig = px.scatter(
-    filtered_data2,
-    x="DaysUntilClose",
-    y="ExpectedNumberOfAwards",
-    size="EstimatedTotalProgramFunding",
-    color="AgencyName",
-    hover_name="AgencyName",
-    size_max=60,
-    title="Bubble Plot of Grant Opportunities by Top Funding Agencies"
-)
-
-fig.show()
-
-# %%
-
-fig.write_html("Bubble.html")  # Replace with your desired file path
-#%%
-
-# %%
-
-from reportlab.lib.pagesizes import letter
-from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, PageBreak, paragraph    
-from reportlab.lib.units import inch
-#%%
-#Create a PDF witht the title "My Title"
--- download_and_clean_raw_data.py
+++ download_and_clean_raw_data.py
@@ -5,6 +5,18 @@ import os
 import requests
 import xml.etree.ElementTree as ET
 import pandas as pd
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib as mpl
+
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from reportlab.lib import colors
+from reportlab.lib.units import inch
+from datetime import datetime
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import Paragraph, Image
+
 
 
 def global_variables():
@@ -104,3 +116,155 @@ def create_log_file(df_clean, today, col):
             f.write(f'There are {df_clean[c].isnull().sum()} null values in {c} column\n')
             f.write(f'There are {df_clean[c].nunique()} unique values in {c} column\n') 
             f.write('\n')
+
+def create_line_chart(df_clean, today_date):
+    # Set the default font to Arial or a similar sans-serif font
+    mpl.rcParams['font.family'] = 'sans-serif'
+    mpl.rcParams['font.sans-serif'] = 'Arial'
+
+    # Assuming df_clean has CloseDate and EstimatedTotalProgramFunding columns
+    # Convert CloseDate to month and filter for the next 12 months
+    df_clean['Month'] = df_clean['CloseDate'].dt.to_period('M')
+    next_12_months = pd.period_range(start=datetime.now(), periods=12, freq='M')
+    df_clean = df_clean[df_clean['Month'].isin(next_12_months)]
+
+    # Group by month and sum the funding
+    monthly_funding = df_clean.groupby('Month')['EstimatedTotalProgramFunding'].sum()
+
+    # Convert funding to millions for the Y axis
+    monthly_funding_in_millions = monthly_funding / 1e6
+
+    # Plotting
+    plt.figure(figsize=(6, 2))  # Adjust the size to fit 1/3rd of the PDF page height
+    plt.plot(monthly_funding_in_millions.index.astype(str), monthly_funding_in_millions.values, 
+            marker='', linestyle='-', linewidth=1)
+    plt.xticks(rotation=45)
+    plt.xlabel('Month')
+    plt.ylabel('In Million')
+    plt.title('Estimated Total Program Funding')
+    plt.gca().spines['top'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+    plt.tight_layout()
+
+    # Save the plot as an image
+    plt.savefig('line_chart.png')
+    plt.close()
+
+def create_pdf(df_clean, today):
+
+    DE = df_clean.query('AgencyName == "Department of Education" \
+              and CloseDate >= @today')
+
+    #This is the method to format and sum the EstimatedTotalProgramFunding column
+    #and print the results to the console for easy sharing.
+    formatted_funding = "${:,.0f}".format(DE["EstimatedTotalProgramFunding"].sum())
+
+    # Define the PDF file name
+    filename = "Current_Opportunities.pdf"
+
+    # Create a canvas
+    c = canvas.Canvas(filename, pagesize=letter)
+
+    # Define title and sub-header
+    title = "Current Opportunities"
+    today_date = datetime.now().strftime("%B %d, %Y")  # Date in long format
+
+    # Add title and sub-header to the canvas
+    c.setFont("Helvetica-Bold", 16)
+    c.drawString(inch, 10*inch, title)
+    c.setFont("Helvetica", 12)
+    c.drawString(inch, 9.75*inch, today_date)
+
+    # Create a paragraph with three generic sentences
+    styles = getSampleStyleSheet()
+    paragraph_text = (
+        "In the realm of educational advancement, the Department of Education consistently offers a multitude of "
+        "grant opportunities, aiming to foster innovation and progress in learning environments. Key values essential "
+        "for the success of these grants include a deep commitment to educational equity, a thorough understanding of "
+        "pedagogical best practices, and a strong alignment of project goals with the Department's vision. Currently, "
+        f"there is a notable Estimated Total Program Funding of {formatted_funding}, accessible as of {today_date}, "
+        "a testament to the Department's dedication to empowering educational initiatives. Successful grant applications "
+        "typically demonstrate not only a robust educational impact but also a sustainable and scalable model, ensuring "
+        "that the benefits of the grant extend beyond the immediate project scope and contribute meaningfully to the "
+        "broader educational landscape."
+    )
+    paragraph = Paragraph(paragraph_text, style=styles["Normal"])
+
+    # Draw the paragraph on the canvas
+    paragraph.wrapOn(c, 6.5*inch, 9*inch)
+    paragraph.drawOn(c, inch, 8*inch)
+
+    chart_image = Image('line_chart.png')
+    chart_image.drawHeight = 3*inch  # Adjust the height to 1/3rd of the page height
+    chart_image.drawWidth = 7*inch  # Adjust the width to fit the page
+    chart_image.wrapOn(c, 7.5*inch, 9*inch)
+    chart_image.drawOn(c, inch, 5*inch)  # Adjust the position as needed
+
+    # Additional paragraph text
+    additional_paragraph_text = ("As we can see, there are millions of dollars in funding available for the next 12 months."
+                                "The chart above shows the estimated total program funding by month for the next 12 months."
+                                "The chart was created using Python and the Pandas and Matplotlib libraries."
+                                "Contact us to learn more about how we can help you with your grant application.")
+
+    # Create a Paragraph object with the additional paragraph
+    additional_paragraph = Paragraph(additional_paragraph_text, style=styles["Normal"])
+
+    # Draw the additional paragraph on the canvas
+    additional_paragraph.wrapOn(c, 6.5*inch, 9*inch)
+    additional_paragraph.drawOn(c, inch, 4*inch)  # Adjust the position as needed
+    # Save the PDF
+    c.save()
+
+
+def create_bubble_plot():
+    # Load the dataset
+    file_path = r"C:\Python\Grants_dot_gov\GrantsDBExtract20211006v2.csv"  # Update with the path to your data file
+    grants_data = pd.read_csv(file_path)
+
+    # Convert CloseDate to datetime and calculate the number of days from today
+    grants_data['CloseDate'] = pd.to_datetime(grants_data['CloseDate'], errors='coerce')
+    today = datetime.now()
+    grants_data['DaysUntilClose'] = (grants_data['CloseDate'] - today).dt.days
+
+    # # Handle NaN values in 'EstimatedTotalProgramFunding'
+    filtered_data = grants_data.dropna(subset=['EstimatedTotalProgramFunding'])
+
+    # Group by AgencyName and sum the EstimatedTotalProgramFunding, then take the top 20
+    top_agencies = filtered_data.groupby('AgencyName')['EstimatedTotalProgramFunding'].sum().nlargest(20).index
+
+    # Filter the dataset for only these top agencies
+    filtered_data = filtered_data[filtered_data['AgencyName'].isin(top_agencies)].query('CloseDate >= @today')    
+    filtered_data2 = filtered_data[filtered_data['DaysUntilClose'] > 0]
+
+    # Create the bubble plot with the adjusted data
+    fig = px.scatter(
+        filtered_data2,
+        x="DaysUntilClose",
+        y="ExpectedNumberOfAwards",
+        size="EstimatedTotalProgramFunding",
+        color="AgencyName",
+        hover_name="AgencyName",
+        size_max=60,
+        title="Bubble Plot of Grant Opportunities by Top Funding Agencies"
+    )
+
+    fig.show()
+
+    # %%
+
+    fig.write_html("Bubble.html")  # Replace with your desired file path
+
+
+def agencies(df_clean):
+    today = datetime.today()
+    agencies = df_clean \
+                .query('CloseDate >= @today') \
+                .groupby('AgencyName') \
+                .agg({'EstimatedTotalProgramFunding': 'sum'}) \
+                .sort_values(by='EstimatedTotalProgramFunding', ascending=False) \
+                .reset_index() \
+                .assign(in_MM = lambda x: x.EstimatedTotalProgramFunding/1000000) 
+                
+    agencies = agencies[agencies['EstimatedTotalProgramFunding'] > 0]
+
+    return agencies
